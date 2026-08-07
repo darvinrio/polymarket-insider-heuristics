f8d6e782c3654b120194870c5bfaa68f1e6c533c
darvin <todarvinrio@gmail.com>
2026-08-07 23:00:01 +0530

    feat: add v2 validation combining stratification and recitification

---
+++ scripts/validation/pnl_check_v2.py
@@ -0,0 +1,120 @@
+import polars as pl
+from loguru import logger
+
+from scripts.validation.closed_positions import get_closed_positions
+from scripts.validation.schema import CLOSED_POSITIONS_SCHEMA, SAMPLE_SCHEMA
+
+logger.success("START")
+
+samples_file = "data/csvs/polymarket_resolutions_stratified_sampling.csv"
+rectified_samples = "data/csvs/polymarket_resolutions_v5_sample_positions_rectified.csv"
+
+keys = ["trader", "token_id", "condition_id"]
+
+samples_df = (
+    pl.scan_csv(samples_file, schema_overrides=SAMPLE_SCHEMA)
+    .with_columns(pl.concat_str(keys, separator="|").alias("key"))
+    .drop(["h", "pnl_bucket", "rn"])
+)
+
+# Trader + List of Condition IDs
+trader_condition_df = (
+    samples_df.select(
+        [
+            pl.col("trader"),
+            pl.col("condition_id"),
+        ]
+    )
+    .unique()
+    .group_by(pl.col("trader"))
+    .agg(
+        pl.col("condition_id").alias("condition_id_list"),
+        pl.col("condition_id").len().alias("condition_id_count"),
+    )
+)
+
+all_positions = []
+for (
+    trader,
+    condition_id_list,
+    condition_id_count,
+) in trader_condition_df.collect().iter_rows():
+    # logger.debug(f"Trader: {trader}, Conditions: {condition_id_count}")
+    trader_closed_positions = get_closed_positions(
+        trader=trader,
+        condition_ids=condition_id_list,
+    )
+    all_positions.extend(trader_closed_positions)
+
+all_positions_df = pl.LazyFrame(all_positions, schema_overrides=CLOSED_POSITIONS_SCHEMA)
+
+
+def get_pnl_df(
+    samples_df: pl.LazyFrame, all_positions_df: pl.LazyFrame
+) -> pl.LazyFrame:
+
+    return (
+        samples_df.join(
+            all_positions_df,
+            left_on=["trader", "token_id"],
+            right_on=["proxyWallet", "asset"],
+            how="left",
+        )
+        .select(
+            "trader", "token_id", "condition_id", "final_profit", "realizedPnl", "key"
+        )
+        .with_columns(
+            pl.col("realizedPnl").sub(pl.col("final_profit")).alias("pnl_diff"),
+            (pl.col("realizedPnl").sub(pl.col("final_profit")))
+            .mul(100.00)
+            .truediv(pl.col("final_profit"))
+            .abs()
+            .alias("pnl_diff_percent"),
+            pl.when(pl.col("realizedPnl").is_null())
+            .then(0)
+            .otherwise(1)
+            .alias("is_available"),
+        )
+        .with_columns(
+            pl.when(pl.col("final_profit").round(2).eq(pl.col("realizedPnl").round(2)))
+            .then(0)
+            .otherwise(pl.col("pnl_diff_percent"))
+            .alias("pnl_diff_percent"),
+            pl.when(pl.col("final_profit").eq(0.00))
+            .then(1)
+            .otherwise(0)
+            .alias("is_inf"),
+        )
+    )
+
+
+combined_df = get_pnl_df(samples_df, all_positions_df)
+
+problematic_df = combined_df.filter(
+    (pl.col("pnl_diff_percent").ge(1)) & (pl.col("pnl_diff").ge(10))
+).sort("pnl_diff_percent", descending=True)
+
+logger.info(f"Problematic positions: {problematic_df.collect().shape[0]}")
+
+## ROUND 2
+logger.info("ROUND 2")
+
+problematic_keys = problematic_df.select("key").collect().to_series().to_list()
+rectified_samples_df = (
+    pl.scan_csv(rectified_samples, schema_overrides=SAMPLE_SCHEMA)
+    .with_columns(pl.concat_str(keys, separator="|").alias("key"))
+    .filter(pl.col("key").is_in(problematic_keys))
+)
+
+rectified_combined_df = get_pnl_df(rectified_samples_df, all_positions_df)
+problematic_rectified_df = rectified_combined_df.filter(
+    (pl.col("pnl_diff_percent").ge(1)) & (pl.col("pnl_diff").ge(10))
+).sort("pnl_diff_percent", descending=True)
+
+logger.info(
+    f"Problematic rectified positions: {problematic_rectified_df.collect().shape[0]}"
+)
+
+problematic_rectified_df.drop("key").collect().write_csv(
+    "scripts/validation/outputs/pnl_diff_percent_above_1_v2.csv"
+)
