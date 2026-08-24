c806b1c9655fb9ec002e88028e55af5d71e84953
darvin <todarvinrio@gmail.com>
2026-08-24 12:51:40 +0530

    chore: prep ledger for only problematic positions

--- scripts/validation/problematic_match_api.py
+++ scripts/validation/problematic_match_api.py
@@ -14,17 +14,32 @@ PROBLEMATIC_USER_ACTIVITY_FILE = (
 )
 LEDGER_FILE = "data/csvs/polymarket_resolutions_v7_full_sample_positions.csv"
 
-api_positions_df = pl.read_csv(
+api_positions_df = pl.scan_csv(
     API_POSITIONS_FILE, schema_overrides=API_POSITIONS_SCHEMA
 )
-problematic_df = pl.read_csv(PROBLEMATIC_FILE, schema_overrides=PROBLEMATIC_FILE_SCHEMA)
-problematic_user_activity_df = pl.read_csv(
+problematic_df = pl.scan_csv(PROBLEMATIC_FILE, schema_overrides=PROBLEMATIC_FILE_SCHEMA)
+problematic_user_activity_df = pl.scan_csv(
     PROBLEMATIC_USER_ACTIVITY_FILE, schema_overrides=USER_ACTIVITY_SCHEMA
 )
-ledger_df = pl.read_csv(LEDGER_FILE, schema_overrides=AUDIT_SCHEMA)
+ledger_df = pl.scan_csv(LEDGER_FILE, schema_overrides=AUDIT_SCHEMA)
 
 # first from ledger, we filter for problematic positions
 # then we run a quick labelling of each position based on
 # number of `convert_from_no` and `transfer_out`
 # we recalculate pnl by ignoring `convert_from_no` and `transfer_out`
 # compare the results with api positions df, see if problematic positions persist
+
+ledger_columns = ledger_df.collect_schema().names()
+problematic_ledger_df = ledger_df.join(
+    problematic_df,
+    left_on=["trader", "token_id"],
+    right_on=["trader", "token_id"],
+    suffix="_right",
+    how="inner",
+).select(ledger_columns)
+
+print(problematic_ledger_df.collect_schema().names())
+
+problematic_ledger_df.collect().write_csv(
+    "scripts/validation/outputs/problematic_ledger_df.csv"
+)
